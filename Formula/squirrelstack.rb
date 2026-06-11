e8a6003b6a32731cc55eb3cbe9fc959467875c98
Rich Aston <rich@aston.io>
2026-06-11 16:51:42 +0100

    Update squirrelstack to v0.4.0

--- Formula/squirrelstack.rb
+++ Formula/squirrelstack.rb
@@ -1,8 +1,8 @@
 class Squirrelstack < Formula
   desc "CLI for SquirrelStack error management"
   homepage "https://github.com/futuremill/squirrelstack-cli"
-  url "https://github.com/futuremill/squirrelstack-cli/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "b3441bf6ea1dea7f53915948bb96031e7b51102f234b6fb5e1fb579afb04947e"
+  url "https://github.com/futuremill/squirrelstack-cli/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "c2e48606114b8de0975a752688e5a9006b342e3b8d9d101cbfef28a11e7e1b4a"
   license :cannot_represent
 
   def install
@@ -10,6 +10,6 @@ class Squirrelstack < Formula
   end
 
   test do
-    assert_match "squirrelstack 0.3.0", shell_output("#{bin}/squirrelstack version")
+    assert_match "squirrelstack 0.4.0", shell_output("#{bin}/squirrelstack version")
   end
 end
